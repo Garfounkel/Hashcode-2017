ebad393ef434002deed8b2b9a55c651882fffff9
Franck Thang <thang_f@epita.fr>
2017-02-23 20:55:08 +0100

    p

--- src/output.cc
+++ src/output.cc
@@ -15,7 +15,7 @@ bool compare_latency(Endpoint& a, Endpoint& b)
 
 void sort_latency_rec(Endpoint a)
 {
- std::sort(a.begin(), a.end(), compare_latency);
+  std::sort(a.begin(), a.end(), compare_latency);
 }
 
 
@@ -27,16 +27,28 @@ void sort_latency(std::vector<Endpoint>& ep)
   }
 }
 
-void global_sort(std::vector<Request>& req, std::vector<Endpoint>& ep)
+void global_sort(std::vector<Request>& req, std::vector<Endpoint>& ep, std::vector<Video>& v, std::vector<Cache>& ch)
 {
   sort_request(req);
   sort_latency(ep);
 
-  std::vector<Cache> vect_cache;
 
   for (auto it = req.begin(); it != req.end(); ++it)
   {
+    int j = 0;
+    bool b = true;
     auto i = std::find(ep.begin(), ep.end(), *it.endpoint_id);
-    i.caches.begin();
+
+    while (b)
+    {
+      if ((i.caches.begin() + j).capacity > v[*it.video_id].size)
+        j++;
+      else
+      {
+        (i.caches.begin() + j).capacity -=  v[*it.video_id].size;
+        (i.caches.begin() + j).out.push_back(v[*it.video_id]);
+        b = false;
+      }
+    }
   }
 }
