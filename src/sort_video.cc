8490d30c4bd9541361d7e2400e5bd1cbd2864894
Hadrien Berthier <berthi_h@epita.fr>
2017-02-23 20:16:29 +0100

    [SORT_REQ]

--- src/sort_video.cc
+++ src/sort_video.cc
@@ -1,7 +1,8 @@
 #include <algorithm>
 #include <vector>
 
-void sort_request(std::vector<Request> req)
+void sort_request(std::vector<Request>& req)
 {
-
+  std::sort(req.begin(), req.end(), [](const Request& lhs, Request& rhs){ return lhs. < rhs.value; });
+  }
 }
