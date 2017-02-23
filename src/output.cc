71fac7bd9dda8331f3fd8d71912990a33fb6a040
Franck Thang <thang_f@epita.fr>
2017-02-23 20:33:57 +0100

    [OUTPUT] doing cache vector

--- src/output.cc
+++ src/output.cc
@@ -3,6 +3,11 @@
 #include "caches.hh"
 
 
+bool operator== (const Endpoint& a, const Endpoint& b)
+{
+  return a.id_ == b.id_;
+}
+
 bool compare_latency(Endpoint& a, Endpoint& b)
 {
   return a.latency < b.latency;
@@ -14,10 +19,24 @@ void sort_latency_rec(Endpoint a)
 }
 
 
-void sort_latency(std::vector<Endpoint>& rq)
+void sort_latency(std::vector<Endpoint>& ep)
 {
   for (auto it = rq.begin(); it != rq.end(); ++it)
   {
     sort_latency_rec(*it);
+  }
+}
+
+void global_sort(std::vector<Request>& req, std::vector<Endpoint>& ep)
+{
+  sort_request(req);
+  sort_latency(ep);
+
+  std::vector<Cache> vect_cache;
+
+  for (auto it = req.begin(); it != req.end(); ++it)
+  {
+    auto i = std::find(ep.begin(), ep.end(), *it.endpoint_id);
+    i.caches.begin();
   }
 }
