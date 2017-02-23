e83971da9bc985734d07f28a7d2a113c1f868579
Franck Thang <thang_f@epita.fr>
2017-02-23 20:15:56 +0100

    [OUTPUT] Sort for latency done

--- src/output.cc
+++ src/output.cc
@@ -2,10 +2,17 @@
 #include <algorithm>
 #include "caches.hh"
 
-void sort_latency_rec(Endpoint)
+
+bool compare_latency(Endpoint& a, Endpoint& b)
 {
-  for (auto it = 
+  return a.latency < b.latency;
 }
+
+void sort_latency_rec(Endpoint a)
+{
+ std::sort(a.begin(), a.end(), compare_latency);
+}
+
 
 void sort_latency(std::vector<Endpoint>& rq)
 {
